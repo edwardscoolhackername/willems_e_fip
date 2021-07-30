dc60873a529ebf310162a350dfafde876290d966
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-07-30 17:12:57 -0400

    Shop page started, responsiveness and buttons added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 (() => {
   const button = document.querySelector('#burgerbutton'),
-        burgercon = document.querySelector('#burgercontainer');
+        burgercon = document.querySelector('#burgercontainer'),
+        quantity = document.querySelectorAll('.forminput');
 
   function showmenu() {
     button.classList.toggle("expanded");
     burgercon.classList.toggle("slideToggle");
   }
 
+  function up(max) {
+    quantity.value = parseInt(quantity.value) + 1;
+    if (quantity.value >= parseInt(max)) {
+        quantity.value = max;
+    }
+}
+function down(min) {
+    quantity.value = parseInt(quantity.value) - 1;
+    if (quantity.value <= parseInt(min)) {
+        quantity.value = min;
+    }
+}
 
   button.addEventListener("click", showmenu);
 })();
