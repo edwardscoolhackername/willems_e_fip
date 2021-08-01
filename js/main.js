c8a469a37cf8d82ff8384f8e2b62493bec309156
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-08-01 15:25:38 -0400

    Shop page responsiveness finished, overlay box for phone-sized screens scripted in

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@
         overlaylabel = document.querySelector('.overlaylabel'),
         overlaydes = document.querySelector('.beerdescrip'),
         close = document.querySelector('.closebutton'),
-        beerpic = document.querySelectorAll('.beerlink');
+        beerpic = document.querySelectorAll('.beerlink'),
+        shoplinks = document.querySelectorAll('.shopclick'),
+        shopbox = document.querySelector('.shopoverlay'),
+        shoplabel = document.querySelector('.shopoverlaylabel'),
+        shopdescrip = document.querySelector('.shopoverlaydescrip');
 
   const send = document.querySelector('.sendbutton'),
         okay = document.querySelector('.okay'),
@@ -20,6 +24,7 @@
     [`fragrance`, `Fragrance-Free Formula Powder`, `Crafted with the finest ingredients that leave your entire body feeling fresh and comfortable in a method that reduces the alcohol but not the flavour. A delicate balance of crisp spices and herb notes in the freshest of non-alcoholic beers.`],
     [`spray`, `Original Formula Spray`, `The perfect blend of subtle spices and herb notes delivered in a light spray that leaves you feeling fresh, comfortable, and smelling great.`]
   ];
+
 
   function saythanks() {
     okaybox.classList.remove('hidden');
@@ -62,10 +67,26 @@
     overlaybox.classList.remove('thereitis');
   }
 
+  function shopopen() {
+    let targetbeer = this.dataset.beer;
+
+    shopbox.classList.remove('hidden');
+    shopinfo(targetbeer);
+  }
+  function shopinfo(targetIndex) {
+    shoplabel.textContent = beerdata[targetIndex][1];
+    shopdescrip.textContent = beerdata[targetIndex][2];
+  }
+  function closeshop() {
+    shopbox.classList.add('hidden');
+  }
+
   button.addEventListener("click", showmenu);
   beerpic.forEach(pic => pic.addEventListener("click", toggleoverlay));
   send.addEventListener("click", saythanks);
   okay.addEventListener("click", sayokay);
   close.addEventListener("click", closebox);
+  shoplinks.forEach(link => link.addEventListener("click", shopopen));
+  close.addEventListener("click", closeshop);
 
 })();
